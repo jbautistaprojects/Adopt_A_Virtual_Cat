a4d88cdfe26e3ffe338eba14c21f47c669eed602
Justina Mandaro <bautistajustina7@gmail.com>
2022-09-19 14:36:53 -0700

    added button id-randCatButton to html, added a click event listener to the randCatButton and created the function upon click titled fetchCat. Current fetchCat only console logs the word click. Source to index.js was added to the bottom of html

---
+++ index.js
@@ -0,0 +1,8 @@
+//fetch cat from API Server
+let randCatBtn = document.getElementById("randCatButton");
+randCatBtn.addEventListener('click', fetchCat);
+
+
+function fetchCat() {
+    console.log('click!');
+}
