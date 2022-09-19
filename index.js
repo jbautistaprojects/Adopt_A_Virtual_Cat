73cce881d19c76dbb082e682f860974ef6e67b2f
Justina Mandaro <bautistajustina7@gmail.com>
2022-09-19 16:40:28 -0700

    created a select table in the html file, created a fetchTag function that obtains the API for the tags on the cat API, create the function renderForm that appends these options to the select upon the page loading.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //fetch cat from API Server
+document.addEventListener('DOMContentLoaded', function() {
 let randCatBtn = document.getElementById("randCatButton");
 randCatBtn.addEventListener('click', fetchCat);
+fetchCatTags();
+});
 
-
+//Functions for fetching API information and rendering it to the DOM
 function fetchCat() {
     fetch("https://cataas.com/api/cats")
   .then(resp => resp.json())
@@ -10,12 +13,31 @@ function fetchCat() {
 
 }
 
+function fetchCatTags() {
+    fetch("https://cataas.com/api/tags")
+    .then(resp => resp.json())
+    .then((json) => renderForm(json))
+}
+
 function renderCat(cats) {
-    console.log(cats)
- 
+    appendCatImg = document.getElementById('pickCat')
+    cats.forEach(cats => {
+        const imgCats = document.createElement('img');
+        imgCats.src = `https://cataas.com/cat/${cats.id}`;
+        appendCatImg.appendChild(imgCats);
+    })
+}
+
+function renderForm(tags) {
+    const catTagsSelect = document.getElementById('catTags');
+    tags.forEach(tags=>{
+        const catOption = document.createElement('option');
+        catOption.value = `${tags}`;
+        catOption.textContent = `${tags}`;
+        catTagsSelect.appendChild(catOption);
+    })
 }
 
 // document.addEventListener('DOMContentLoaded', function() {
-//   fetchBooks();
-//   console.log("DOM loaded")
+//   
 // });
